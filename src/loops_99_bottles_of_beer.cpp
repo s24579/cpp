ba4e46a166132fb8f91194d01d90f07a4ba1c2d1
s24579 <s24579@pjwstk.edu.pl>
2021-10-26 09:28:59 +0200

    esa

--- src/loops_99_bottles_of_beer.cpp
+++ src/loops_99_bottles_of_beer.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    int x,y;
-    std::cout << "Type number: ";
-    std::cin >> x;
-    y=x;
-    if(x<0){
-        std::cout << "Number is negative\n";
-    }
-    do{
+auto main(int argc, char* argv[])-> int{
+    if (argc == 0) {
+        int x=99;
+        do{
         std::cout << x << " bottles of beer on the wall, " << x << " bottles of beer.\n";
         x--;
         std::cout << "Take one down, pass it aroud, " << x << " bottles of beer on the wall...\n";
     }while(x>0);
-    std::cout << "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store an but some more, " << y << " bottles of beer on the wall...\n";
+    std::cout << "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store an but some more, 99 bottles of beer on the wall...\n";
 
+    }
+    else{
+    int y=std::stoi(argv[1]);
+    if(y<0){
+        std::cout << "Number is negative\n";
+    }
+    do{
+        std::cout << y << " bottles of beer on the wall, " << y << " bottles of beer.\n";
+        y--;
+        std::cout << "Take one down, pass it aroud, " << y << " bottles of beer on the wall...\n";
+    }while(y>0);
+    std::cout << "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store an but some more, 99 bottles of beer on the wall...\n";
+    }
     return 0;
 }
 /* Wypisuje tekst piosenki do 0 */
--- src/test_argv.cpp
+++ src/test_argv.cpp
@@ -5,5 +5,6 @@ auto main(int argc, char* argv[])-> int{
     if (argc == 0) {
         return 1;
     }
+    std::cout << argv[1];
 return 0;
 }
