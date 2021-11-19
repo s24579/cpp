2d475a70af7424f42e4201f1b2be79f41f140580
s24579 <s24579@pjwstk.edu.pl>
2021-11-19 09:42:18 +0100

    kanessa

--- src/3_1_1_casual.cpp
+++ src/3_1_1_casual.cpp
@@ -8,4 +8,17 @@ auto init(int a[], int n)-> void
     }
 return;
 }
+auto main(int argc , char* argv[]) -> int
+{
+    int n=std::stoi(argv[1]);
+    if(argc==0)
+    {
+        return 1;
+    }
+    else
+    {
+        std::cout << n << std::endl;
+    }
+    return 0;
+}
 /* NIE DOKONCZONE */
