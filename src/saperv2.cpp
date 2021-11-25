e412b9a10b3e531fc932307c3210307450bbfee4
s24579 <91719568+s24579@users.noreply.github.com>
2021-11-25 21:06:37 +0100

    sa

--- src/saperv2.cpp
+++ src/saperv2.cpp
@@ -3,6 +3,7 @@
 int mapa[30][30]={};
 int kolumna;
 int wiersz;  
+int essa;
     auto wypisanie_mapy() -> void
     {
         for(int i=0;i<30;i++)
@@ -18,42 +19,124 @@ int wiersz;
 
     auto dodanie_cyfr() -> void
     {   
-        mapa[0][1]=9;
-        mapa[0][5]=9;
-        mapa[0][6]=9;
-        for(int i=0;i<30;i++)
+        
+        int kol,wier;
+        for(int i=1;i<29;i++)
         {
-            for(int j=0;j<30;j++)
+            for(int j=1;j<29;j++)
             {   
                 if(mapa[i][j]==9)
-                {
-                    if(i==0&&j==0)
-                    {
-                        mapa[i+1][j]+=1;
-                        mapa[i+1][j+1]+=1;
-                        mapa[i][j+1]+=1;
-                    }
-                    else if(i==0)
-                    {
-                        mapa[i][j-1]+=1;
-                        mapa[i+1][j-1]+=1;
-                        mapa[i+1][j]+=1;
-                        mapa[i+1][j+1]+=1;
-                        mapa[i][j+1]+=1;
+                {   
+                    kol=i-1;
+                    wier=j-1;
+                    for(int fl=0;fl<3;fl++)
+                    {   
+                        
+                        for(int fl2=0;fl2<3;fl2++)
+                        {   
+                            if(mapa[kol][wier]!=9)
+                            {
+                                mapa[kol][wier]+=1;
+                            }
+                            kol++;
+                        }
+                        wier++;
+                        kol=i-1;
                     }
                 }
             }
         }
-        return; 
+        for(int g=1;g<29;g++)
+        {
+            if(mapa[0][g]==9)
+            {   
+                mapa[0][g-1]+=1;
+                mapa[1][g-1]+=1;
+                mapa[1][g]+=1;
+                mapa[1][g+1]+=1;
+                mapa[0][g+1]+=1;
+            }
+        }
+        for(int p=1;p<29;p++)
+        {
+            if(mapa[0][g]==9)
+            {   
+                mapa[0][g-1]+=1;
+                mapa[1][g-1]+=1;
+                mapa[1][g-1]+=1;
+                mapa[1][g]+=1;
+                mapa[1][g+1]+=1;
+            }
+        }
+        if(mapa[0][0]==9)
+        {
+            if(mapa[1][0]!=9)
+            {
+            mapa[1][0]+=1;
+            }
+            if(mapa[1][1]!=9)
+            {
+            mapa[1][1]+=1;
+            }
+            if(mapa[0][1]!=9)
+            {
+            mapa[0][1]+=1;
+            }        
+        }
+        if(mapa[29][29]==9)
+        {
+            if(mapa[29][28]!=9)
+            {
+            mapa[29][28]+=1;
+            }
+            if(mapa[28][29]!=9)
+            {
+            mapa[28][29]+=1;
+            }
+            if(mapa[28][28]!=9)
+            {
+            mapa[28][28]+=1;
+            }
+        }
+        if(mapa[0][29]==9)
+        {   
+            if(mapa[0][28]!=9)
+            {
+            mapa[0][28]+=1;
+            }
+            if(mapa[1][28]!=9)
+            {
+            mapa[1][28]+=1;
+            }
+            if(mapa[1][29]!=9)
+            {
+            mapa[1][29]+=1;
+            }
+        }
+        if(mapa[29][0]==9)
+        {
+            if(mapa[28][0]!=9)
+            {
+            mapa[28][0]+=1;
+            }
+            if(mapa[28][1]!=9)
+            {
+            mapa[28][1]+=1;
+            }
+            if(mapa[29][1]!=9)
+            {
+            mapa[29][1]+=1;
+            }
+        }
     }
 
     auto bomby() -> void
     {   
         std::random_device rd;
-        std::uniform_int_distribution<int>random(0,29);
+        std::uniform_int_distribution<int>random(1,28);
         kolumna=random(rd);
         wiersz=random(rd);
-        for(int x=0;x<15;)
+        for(int x=0;x<100;)
         {   
             if(mapa[kolumna][wiersz]==0)
             {
@@ -70,7 +153,7 @@ int wiersz;
     
 auto main() ->int 
 {
-    //bomby();
+    bomby();
     dodanie_cyfr();
     wypisanie_mapy();
     return 0;
